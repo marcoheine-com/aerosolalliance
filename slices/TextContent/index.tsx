959be503d32be112acea0573cff4c5d63d59a223
Marco Kühbauch <marcokuehbauch@gmail.com>
2022-05-24 11:21:50 +0200

    adjust text styles

--- slices/TextContent/index.tsx
+++ slices/TextContent/index.tsx
@@ -20,16 +20,7 @@ const TextContent: FunctionComponent<Props> = ({ slice }): JSX.Element => {
           key={i}
           className="px-6 mx-auto max-w-4xl sm:px-16 module-spacing-b textContent"
         >
-          <PrismicRichText
-            field={item.Content}
-            components={{
-              paragraph: (props: any) => (
-                <p className="leading-[2.1] xl:leading-[2.75]">
-                  {props.children}
-                </p>
-              ),
-            }}
-          />
+          <PrismicRichText field={item.Content} />
         </section>
       ))}
     </>
